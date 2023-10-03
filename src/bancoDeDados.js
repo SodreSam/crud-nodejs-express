e84d2814e60943f38563b491c5a3d6b3536672d7
Samuel Sodré <sodrer.hsamuel@gmail.com>
2023-10-02 23:12:16 -0300

    Estrutura base para um CRUD em NodeJS

---
+++ src/bancoDeDados.js
@@ -0,0 +1,18 @@
+let alunos = [
+    {
+      id: 1,
+      nome: "Samuel",
+      email: "samuel@teste.com",
+      senha: "123",
+    },
+    {
+      id: 2,
+      nome: "Pedro",
+      email: "pedro@teste.com",
+      senha: "4321",
+    },
+  ];
+  
+  module.exports = {
+    alunos,
+  };
